a2c9e7c4c871c7fc02126c0620f3a638a48741e2
yejinkim <yejin-kim@yonsei.ac.kr>
2024-10-10 18:09:57 +0900

    feat:search page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:luckybiky/screens/home.dart';
 import 'package:luckybiky/screens/search.dart';
 import 'package:luckybiky/screens/profile.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 
 void main() {
+  await _initialize;
   runApp(SplashScreen());
+}
+
+// 지도 초기화하기
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+    clientId: '<client id>',     // 클라이언트 ID 설정
+    onAuthFailed: (ex) => log("****네이버맵 인증오류 : $ex", name: "onAuthFailed")
+  );
 }
 
 class SplashScreen extends StatelessWidget {
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -10,6 +13,19 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+  final Completer<NaverMapController> _mapControllerCompleter = Completer();
+
+  return Scaffold(
+    appBar: AppBar(
+        title: const Text("Naver Map Example"),
+      ),
+    body: NaverMap(
+      options: const NaverMapViewOptions(),
+      onMapReady: (controller) {
+        _mapControllerCompleter.complete(controller);
+        print("NaverMap Loading complete!");
+      },
+    ),
+  );
   }
 }
