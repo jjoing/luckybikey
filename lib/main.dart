7db95787931701d53294b298e411ae8fc04731d7
jjoing <yoonbincho@yonsei.ac.kr>
2024-10-17 22:32:31 +0900

    feat: use provider with profile preference function

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,30 @@ import 'package:flutter/material.dart';
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:provider/provider.dart';
 
 import 'package:luckybiky/screens/home.dart';
 import 'package:luckybiky/screens/search.dart';
-import 'package:luckybiky/screens/profile.dart';
+import 'package:luckybiky/screens/profileScreen/profile.dart';
+import 'package:luckybiky/screens/profileScreen/preference_provider.dart';
 
 
 
 void main() async {
   await _initialize();
-  runApp(SplashScreen());
+  runApp(
+      ChangeNotifierProvider(
+        create: (context) => PreferenceProvider(),
+        child: SplashScreen(),
+      ),
+  );
 }
 
 Future<void> _initialize() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NaverMapSdk.instance.initialize(
-    clientId: '**********',
-  onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
+      clientId: 'zuupv435hj',
+      onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
   );
 }
 
@@ -94,4 +101,3 @@ class _mainHomeState extends State<mainHome> {
     });
   }
 }
-
