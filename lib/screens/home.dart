1292c910f6000d2aed6e4ab0bd480b0cc7fc1a59
jjoing <yoonbincho@yonsei.ac.kr>
2024-10-24 19:37:41 +0900

    feat: naver map marker

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+
+import 'package:luckybiky/contents/way_sample_data.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,11 +13,55 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<NLatLng> sampleData3Coords = Sample_Data_3.map((point) {
+    return NLatLng(point['lat'], point['lon']);
+  }).toList();
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
+    final Completer<NaverMapController> _mapControllerCompleter = Completer();
 
+    return NaverMap(
+        options: const NaverMapViewOptions(
+            mapType: NMapType.basic,
+            activeLayerGroups: [
+              NLayerGroup.bicycle,
+              // NLayerGroup.traffic,
+              // NLayerGroup.transit,
+            ],
+            initialCameraPosition: NCameraPosition(
+                target: NLatLng(37.525313, 126.9226753),
+                zoom: 12,
+                bearing: 0,
+                tilt: 0
+            ),
+            locationButtonEnable: true,
+            contentPadding: EdgeInsets.all(10)// default : [NLayerGroup.building]
+        ),
+        forceGesture: true,
+        onMapReady: (controller) async {
+          _mapControllerCompleter.complete(controller);
+
+          final path = NPathOverlay(
+            id: 'samplePath3',
+            coords: sampleData3Coords,  // NLatLng로 변환된 좌표 리스트
+            color: Colors.blue,
+            width: 5,
+          );
+
+          const LatLng1 = NLatLng(37.525313, 126.9226753);
+
+          final marker = NMarker(
+            id: 'testMarker',
+            position: LatLng1,  // 서울 좌표
+          );
+
+          controller.addOverlay(marker);
+          controller.addOverlay(path);
+
+        }
     );
   }
 }
--- lib/screens/searchScreen/search.dart
+++ lib/screens/searchScreen/search.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:csv/csv.dart';
 
 import 'package:luckybiky/screens/searchScreen/modal.dart';
+import 'package:luckybiky/contents/way_sample_data.dart';
+
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -13,7 +16,18 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+
+
   @override
+  void initState() {
+    super.initState();
+  }
+
+  List<NLatLng> sampleData3Coords = Sample_Data_3.map((point) {
+    return NLatLng(point['lat'], point['lon']);
+  }).toList();
+
+    @override
   Widget build(BuildContext context) {
     final Completer<NaverMapController> _mapControllerCompleter = Completer();
 
@@ -74,7 +88,9 @@ class _SearchState extends State<Search> {
                   SizedBox(
                     width: 80,
                     child: IconButton(
-                      onPressed: () => {},
+                      onPressed: () async {
+
+                        },
                       icon: Image.asset('assets/images/share_bike_logo.jpeg'),
                     ),
                   ),
@@ -91,13 +107,39 @@ class _SearchState extends State<Search> {
                       mapType: NMapType.basic,
                       activeLayerGroups: [
                         NLayerGroup.bicycle,
-                        NLayerGroup.transit
+                        // NLayerGroup.transit,
                       ],
-                      contentPadding: EdgeInsets.all(10)// default : [NLayerGroup.building]
+                        initialCameraPosition: NCameraPosition(
+                            target: NLatLng(37.525313, 126.9226753),
+                            zoom: 12,
+                            bearing: 0,
+                            tilt: 0
+                        ),
+                        locationButtonEnable: true,
+                        contentPadding: EdgeInsets.all(10)// default : [NLayerGroup.building]
                     ),
                     forceGesture: true,
-                    onMapReady: (controller) {
+                    onMapReady: (controller) async {
+                      await _mapControllerCompleter.future;
                       _mapControllerCompleter.complete(controller);
+
+                      final path = NPathOverlay(
+                        id: 'samplePath3',
+                        coords: sampleData3Coords,  // NLatLng로 변환된 좌표 리스트
+                        color: Colors.blue,
+                        width: 5,
+                      );
+
+                      print(sampleData3Coords);
+
+                      final marker = NMarker(
+                        id: 'testMarker',
+                        position: NLatLng(37.525313, 126.9226753),  // 서울 좌표
+                      );
+
+                      controller.addOverlay(marker);
+                      controller.addOverlay(path);
+
                     }
                 ),
               ),
