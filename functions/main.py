b3f884000b010e5fb0542e8fafce56457af80dad
Joonkyu Jang <2021193003@yonsei.ac.kr>
2024-11-29 02:38:28 +0900

    chore: fixed updated api

--- functions/main.py
+++ functions/main.py
@@ -29,9 +29,9 @@ def get_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
 
 class Node:
     def __init__(self, id: int, geometry: Dict[str, float], connections, parent=None):
-        self.id = id
-        self.lat = geometry["lat"]
-        self.lon = geometry["lon"]
+        self.id: int = id
+        self.lat: float = geometry["lat"]
+        self.lon: float = geometry["lon"]
         self.connections = connections
         self.g: float = 0
         self.h: float = 90000
@@ -40,6 +40,9 @@ class Node:
 
     def __lt__(self, other):
         return self.f < other.f
+
+    def __eq__(self, other):
+        return self.id == other.id
 
 
 def heuristic_Manhattan_distance(cur_node: Node, end_node: Node) -> float:
@@ -84,7 +87,7 @@ def astar_road_finder(
         cur_node = heapq.heappop(open_list)
         closed_set.add(cur_node.id)
 
-        if cur_node.id == end_node.id:
+        if cur_node == end_node:
             final_road = []
             total_distance = cur_node.g
             while cur_node is not None:
@@ -96,8 +99,8 @@ def astar_road_finder(
             new_node = create_node(collection_ref, id)
             if new_node.id in closed_set:
                 continue
-            if new_node.id in [nd.id for nd in open_list]:
-                new_node = [nd for nd in open_list if nd.id == new_node.id][0]
+            if new_node in open_list:
+                new_node = open_list[open_list.index(new_node)]
                 if (cur_node.g + inner_dict["distance"]) >= new_node.g:
                     continue
             new_node.g = cur_node.g + inner_dict["distance"]
@@ -107,10 +110,9 @@ def astar_road_finder(
                 new_node.h = heuristic_Manhattan_distance(new_node, end_node)
             new_node.f = new_node.g + new_node.h
             new_node.parent = cur_node
-            if new_node.id not in [nd.id for nd in open_list]:
-                heapq.heappush(open_list, new_node)
+            heapq.heappush(open_list, new_node)
 
-    # 길이 연결되지 않았으면
+    # 길이 연결되지 않았으면 에러 발생
     raise https_fn.HttpsError(
         code=https_fn.FunctionsErrorCode.INTERNAL,
         message="No route was found between the start and end points.",
@@ -121,10 +123,10 @@ def get_nearest_node(collection_ref: CollectionReference, lat: float, lon: float
     # 선형 검색으로 가장 가까운 노드 탐색. TODO 더 가까운 알고리즘 있으면 대체할 것
     # 기준 좌표 부근에서 후보 노드들 query
     query_start = (
-        collection_ref.where(filter=FieldFilter("lat", ">=", lat - 0.002))
-        .where(filter=FieldFilter("lat", "<=", lat + 0.002))
-        .where(filter=FieldFilter("lon", "<=", lon + 0.002))
-        .where(filter=FieldFilter("lon", ">=", lon - 0.002))
+        collection_ref.where(filter=FieldFilter("lat", ">=", lat - 0.005))
+        .where(filter=FieldFilter("lat", "<=", lat + 0.005))
+        .where(filter=FieldFilter("lon", "<=", lon + 0.005))
+        .where(filter=FieldFilter("lon", ">=", lon - 0.005))
     )
     docs = [doc for doc in query_start.stream()]
 
@@ -160,7 +162,8 @@ def create_node_map(collection_ref: CollectionReference) -> dict[int, Node]:
 def create_node(collection_ref: CollectionReference, id: int) -> Node:
     # Firestore에서 노드 생성
     doc = collection_ref.document(str(id)).get()
-    node = Node(id, doc.to_dict(), doc.to_dict()["connections"])
+    inner_dict = doc.to_dict()
+    node = Node(int(id), {"lat": inner_dict["lat"], "lon": inner_dict["lon"]}, inner_dict["connections"])
     return node
 
 
